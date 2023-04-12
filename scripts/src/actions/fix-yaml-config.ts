abe4b765371e41550b51cc8f678715f9ece8f5da
galargh <piotr.galar@gmail.com>
2023-04-12 10:21:52 +0200

    do not enforce admins on default branches

--- scripts/src/actions/fix-yaml-config.ts
+++ scripts/src/actions/fix-yaml-config.ts
@@ -1,5 +1,8 @@
 import 'reflect-metadata'
 import {Repository} from '../resources/repository'
+import {RepositoryBranchProtectionRule} from '../resources/repository-branch-protection-rule'
+import {globToRegex} from '../utils'
+import {doNotEnforceAdmins} from './do-not-enforce-admins'
 import {addFileToAllRepos} from './shared/add-file-to-all-repos'
 import {format} from './shared/format'
 
@@ -11,8 +14,14 @@ const uninitialisedRepositoryNames = [
 ]
 
 addFileToAllRepos(
-  '.github/workflows/stale.yml', 
   '.github/workflows/stale.yml',
-  (repository: Repository) => !uninitialisedRepositoryNames.includes(repository.name)
+  '.github/workflows/stale.yml',
+  (repository: Repository) =>
+    !uninitialisedRepositoryNames.includes(repository.name)
+)
+doNotEnforceAdmins(
+  (repository: Repository, rule: RepositoryBranchProtectionRule) =>
+    repository.default_branch !== undefined &&
+    globToRegex(rule.pattern).test(repository.default_branch)
 )
 format()
